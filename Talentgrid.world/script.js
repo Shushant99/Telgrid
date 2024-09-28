28dca83ad6b27975035f34714314f5de19368c48
Shushant99 <sushant6683@gmail.com>
2024-09-28 19:03:24 +0530

    Add files via upload

---
+++ Talentgrid.world/script.js
@@ -0,0 +1,5 @@
+// Added type animation 
+var typed = new Typed('#element', {
+    strings: ['Hiring Employees', 'Building Strong Resume', 'Gain Attention','Building strong team','Find team members'],
+    typeSpeed: 50,
+  });
