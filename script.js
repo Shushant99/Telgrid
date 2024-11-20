ae1df326e4af6b20e1bf5df172e41833eb745292
Shushant99 <sushant6683@gmail.com>
2024-11-20 18:48:37 +0530

    d

--- script.js
+++
@@ -1,5 +0,0 @@
-// Added type animation 
-var typed = new Typed('#element', {
-    strings: ['Hiring Employees', 'Building Strong Resume', 'Gain Attention','Building strong team','Find team members'],
-    typeSpeed: 50,
-  });
